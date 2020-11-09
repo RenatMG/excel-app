e3a85c5244eac6d015fc30832b8de2dd0d44531b
Renat <r.detta@yandex.ru>
2020-11-09 13:12:45 +0400

    finish css layout

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-import Person from './person.js';
-import './styles/style.css';
-import './styles/style.scss';
-import pic from './images/3dwall66.jpg';
+import './scss/index.scss';
 
-
-console.log('webpack is working');
-const Elena = new Person('Елена', 35, 'Russia');
-console.log(Elena)
-
-document.querySelector('img').src = pic
+console.log('hello');
