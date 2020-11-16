5e0fbaee8fb3b2cbe161cc3b80949411c34592a9
Renat <r.detta@yandex.ru>
2020-11-16 16:10:58 +0400

    Finish Table resize

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,53 +1,75 @@
 class Dom {
-    constructor(selector) {
-        this.$el = typeof selector === 'string'
-            ? document.querySelector(selector)
-            : selector;
-    }
+	constructor(selector) {
+		this.$el = typeof selector === 'string'
+			? document.querySelector(selector)
+			: selector;
+	}
 
-    html(html) {
-        if (typeof html === 'string') {
-            this.$el.innerHTML = html;
-            return this;
-        }
-        return this.$el.outerHTML.trim();
-    }
+	html(html) {
+		if (typeof html === 'string') {
+			this.$el.innerHTML = html;
+			return this;
+		}
+		return this.$el.outerHTML.trim();
+	}
 
-    clear() {
-        return this.html('');
-    }
+	clear() {
+		return this.html('');
+	}
 
-    on(eventType, callback) {
-        this.$el.addEventListener(eventType, callback);
-    }
+	on(eventType, callback) {
+		this.$el.addEventListener(eventType, callback);
+	}
 
-    off(eventType, callback) {
-        this.$el.removeEventListener(eventType, callback);
-    }
+	off(eventType, callback) {
+		this.$el.removeEventListener(eventType, callback);
+	}
 
-    append(node) {
-        let element = node;
-        if (node instanceof Dom) {
-            element = node.$el;
-        }
+	append(node) {
+		let element = node;
+		if (node instanceof Dom) {
+			element = node.$el;
+		}
 
-        if (Element.prototype.append) {
-            this.$el.append(element);
-        } else {
-            this.$el.appendChild(element);
-        }
-        return this;
-    }
+		if (Element.prototype.append) {
+			this.$el.append(element);
+		} else {
+			this.$el.appendChild(element);
+		}
+		return this;
+	}
+
+	closest(selector) {
+		return this.$el.closest(selector);
+	}
+
+	getCoords() {
+		return this.$el.getBoundingClientRect();
+	}
+
+	findAll(selector) {
+		return this.$el.querySelectorAll(selector);
+	}
+
+	css(styles = {}) {
+		Object.keys(styles).forEach((style) => {
+			this.$el.style[style] = styles[style];
+		});
+	}
+
+	get data() {
+		return this.$el.dataset;
+	}
 }
 
 export function $(selector) {
-    return new Dom(selector);
+	return new Dom(selector);
 }
 
 $.create = (tagName, classes = '') => {
-    const el = document.createElement(tagName);
-    if (classes) {
-        el.className = classes;
-    }
-    return $(el);
+	const el = document.createElement(tagName);
+	if (classes) {
+		el.className = classes;
+	}
+	return $(el);
 };
